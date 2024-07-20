22a5164b419d80442b52cff88f71ba6ee287e69c
ducbao <truongducbao290402@gmail.com>
2024-07-20 22:03:31 +0700

    first commit

---
+++ src/main/java/ducbao/vn/mongdbdemo/MongDbDemoApplication.java
@@ -0,0 +1,12 @@
+package ducbao.vn.mongdbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongDbDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongDbDemoApplication.class, args);
+    }
+}
---
+++ src/test/java/ducbao/vn/mongdbdemo/MongDbDemoApplicationTests.java
@@ -0,0 +1,11 @@
+package ducbao.vn.mongdbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongDbDemoApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
